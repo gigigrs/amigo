dbc173d97bc34fc07dd67472fa7ba5cbc96dfc29
gigigrs <137023786+gigigrs@users.noreply.github.com>
2024-10-07 21:58:58 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
-import {View, Text} from 'react-native'
+import {View, Text, ActivityIndicator} from 'react-native'
 import React from 'react'
 
-export default function Home(){
+export default function StartPage(){
     return(
-        <View style= {{paddingTop: 40}}>
-            <Text>Home</Text>
+        <View className="flex-1 justify-center items-center">
+           <ActivityIndicator size="large" color="gray" />
         </View>
     )
 }
